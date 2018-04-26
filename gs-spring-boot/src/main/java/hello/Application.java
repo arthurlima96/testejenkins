16d96bd745cb26bf8a729a5c60f03892020d364f
Arthur C. Lima <arthur@ocidental.net>
2018-04-26 12:33:26 -0300

    Segundo Commit

--- gs-spring-boot/src/main/java/hello/Application.java
+++ gs-spring-boot/src/main/java/hello/Application.java
@@ -5,15 +5,22 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Override
+   	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+   		return builder.sources(Application.class);
+   	}
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
