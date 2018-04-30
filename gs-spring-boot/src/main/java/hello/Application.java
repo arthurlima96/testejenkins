cb5ccd2b610f4f530c4609327979a7f2be305b40
Arthur C. Lima <arthur@ocidental.net>
2018-04-30 08:44:35 -0300

    teste três

--- gs-spring-boot/src/main/java/hello/Application.java
+++ gs-spring-boot/src/main/java/hello/Application.java
@@ -25,7 +25,8 @@ public class Application extends SpringBootServletInitializer{
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-
+        	
+        	System.out.println("Funcionou");
             System.out.println("Let's inspect the beans provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
